fbb8a09e73fe8862fe92ae618149cc3136cd981a
Matt Stine <mstine@pivotal.io>
2018-03-30 10:44:24 -0500

    store can manage truck inventory

--- src/main/java/com/mattstine/rentaltrucks/inventory/Store.java
+++ src/main/java/com/mattstine/rentaltrucks/inventory/Store.java
@@ -1,7 +1,37 @@
 package com.mattstine.rentaltrucks.inventory;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * @author Matt Stine
  */
 public class Store {
+
+	private Map<Integer, Set<Truck>> trucksByType = new HashMap<>();
+
+	public int trucksOnHand(int typeId) {
+		if (trucksByType.get(typeId) == null) {
+			throw new IllegalArgumentException();
+		}
+
+		return trucksByType.get(typeId).size();
+	}
+
+	public void addTruck(Truck truck) {
+		int typeId = truck.getTypeId();
+		trucksByType.computeIfAbsent(typeId, k -> new HashSet<>());
+
+		trucksByType
+				.get(typeId)
+				.add(truck);
+	}
+
+	public void removeTruck(Truck truck) {
+		trucksByType
+				.get(truck.getTypeId())
+				.remove(truck);
+	}
 }
---
+++ src/test/java/test/com/mattstine/rentaltrucks/inventory/StoreTests.java
@@ -0,0 +1,44 @@
+package test.com.mattstine.rentaltrucks.inventory;
+
+import com.mattstine.rentaltrucks.inventory.Store;
+import com.mattstine.rentaltrucks.inventory.Truck;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Matt Stine
+ */
+public class StoreTests {
+
+	private static final int TYPE_15_FOOT = 0;
+	private Store store;
+
+	@Before
+	public void setUp() {
+		store = new Store();
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void cannotManageTruckType() {
+		store.trucksOnHand(TYPE_15_FOOT);
+	}
+
+	@Test
+	public void canAddATruckToInventory() {
+		store.addTruck(new Truck(TYPE_15_FOOT, true, store));
+		assertThat(store.trucksOnHand(TYPE_15_FOOT), is(equalTo(1)));
+	}
+
+	@Test
+	public void canRemoveATruckFromInventory() {
+		Truck truck = new Truck(TYPE_15_FOOT, true, store);
+		store.addTruck(truck);
+		store.removeTruck(truck);
+		assertThat(store.trucksOnHand(TYPE_15_FOOT), is(equalTo(0)));
+	}
+
+}
