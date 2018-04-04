8e83060961ff9f52eb4059b1f22976ddceeaa9c9
David Wadden <dwadden@pivotal.io>
2018-04-03 19:02:35 -0500

    Adds Spring Boot to project

---
+++ src/test/java/test/com/mattstine/rentaltrucks/RentalTrucksApplicationTests.java
@@ -0,0 +1,16 @@
+package test.com.mattstine.rentaltrucks;
+
+import com.mattstine.rentaltrucks.RentalTrucksApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = RentalTrucksApplication.class)
+public class RentalTrucksApplicationTests {
+
+    @Test
+    public void contextLoads() {}
+
+}
